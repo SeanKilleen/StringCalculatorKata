faa3ff6bdec796b278ab83b4d7b39e4245844005
Sean Killeen <SeanKilleen@gmail.com>
2016-02-02 14:25:35 -0500

    Add class and initial tests

--- StringCalculatorKata/Class1.cs
+++ StringCalculatorKata/Class1.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace StringCalculatorKata
 {
-    public class Class1
+    [TestClass]
+    public class StringCalculatorTests
     {
+        public class Add
+        {
+            [TestMethod]
+            public void EmptyString_ShouldReturnZero()
+            {
+                var sut = new StringCalculator();
+                var result = sut.Add(string.Empty);
+
+                Assert.Equals(result, 0);
+            }
+        }
+
+        public class StringCalculator
+        {
+            public int Add(string numberString)
+            {
+                return 0;
+            }
+        }
     }
 }
